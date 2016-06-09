62429f288377209c25e7a1059a3fd913f6f7cdac
daniel de oliveira <danieloliveira.mg@gmail.com>
2016-06-09 06:47:14 -0300

    Ajustes

--- CastTV/app/src/main/java/com/example/admin/casttv/activitys/MainActivity.java
+++ CastTV/app/src/main/java/com/example/admin/casttv/activitys/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.admin.casttv;
+package com.example.admin.casttv.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.admin.casttv.R;
 
 public class MainActivity extends AppCompatActivity {
 
